b647bb7cf73ec1614a347fb8a542a457544a06b1
unknown <julijana.kolarec@fer.hr>
2024-01-05 14:58:22 +0100

    do uključivo 6. te 14.

--- FRISCGenerator2.py
+++ FRISCGenerator2.py
@@ -4,8 +4,7 @@ import copy
 
 sys.stdout = open('a.frisc', 'w')
 
-with open('helper.txt', 'r') as functions:
-    content = functions.read()
+content = ['MD_SGN\tMOVE 0, R6', '\tXOR R0, 0, R0', '\tJP_P MD_TST1', '\tXOR R0, -1, R0', '\tADD R0, 1, R0', '\tMOVE 1, R6', 'MD_TST1 XOR R1, 0, R1', '\tJP_P MD_SGNR', '\tXOR R1, -1, R1', '\tADD R1, 1, R1', '\tXOR R6, 1, R6', 'MD_SGNR RET', 'MD_INIT POP R4 ; MD_INIT ret addr', '\tPOP R3 ; M/D ret addr', '\tPOP R1 ; op2', '\tPOP R0 ; op1', '\tCALL MD_SGN', '\tMOVE 0, R2 ; init rezultata', '\tPUSH R4 ; MD_INIT ret addr', '\tRET', 'MD_RET  XOR R6, 0, R6 ; predznak?', '\tJP_Z MD_RET1', '\tXOR R2, -1, R2 ; promijeni predznak', '\tADD R2, 1, R2', 'MD_RET1 POP R4 ; MD_RET ret addr', '\tPUSH R2 ; rezultat', '\tPUSH R3 ; M/D ret addr', '\tPUSH R4 ; MD_RET ret addr', '\tRET', 'MUL \tCALL MD_INIT', '\tXOR R1, 0, R1', '\tJP_Z MUL_RET ; op2 == 0', '\tSUB R1, 1, R1', 'MUL_1 \tADD R2, R0, R2', '\tSUB R1, 1, R1', '\tJP_NN MUL_1 ; >= 0?', 'MUL_RET CALL MD_RET', '\tRET', 'DIV \tCALL MD_INIT', '\tXOR R1, 0, R1', '\tJP_Z DIV_RET ; op2 == 0', 'DIV_1 \tADD R2, 1, R2', '\tSUB R0, R1, R0', '\tJP_NN DIV_1', '\tSUB R2, 1, R2', 'DIV_RET CALL MD_RET', '\tRET']
 
 print("\tMOVE 40000, R7 ; init stog\n")
 
@@ -13,7 +12,9 @@ print("\tMOVE 40000, R7 ; init stog\n")
 
 print_variables = []
 
-data = ['<program>', ' <lista_naredbi>', '  <naredba>', '   <naredba_pridruzivanja>', '    IDN 1 rez', '    OP_PRIDRUZI 1 =', '    <E>', '     <T>', '      <P>', '       BROJ 1 17', '      <T_lista>', '       OP_DIJELI 1 /', '       <T>', '        <P>', '         BROJ 1 8', '        <T_lista>', '         $', '     <E_lista>', '      $', '  <lista_naredbi>', '   $']
+data = sys.stdin.read().split("\n")
+
+#data = ['<program>', ' <lista_naredbi>', '  <naredba>', '   <naredba_pridruzivanja>', '    IDN 1 rez', '    OP_PRIDRUZI 1 =', '    <E>', '     <T>', '      <P>', '       BROJ 1 17', '      <T_lista>', '       OP_DIJELI 1 /', '       <T>', '        <P>', '         BROJ 1 8', '        <T_lista>', '         $', '     <E_lista>', '      $', '  <lista_naredbi>', '   $']
 
 data.pop()
 
@@ -50,41 +51,47 @@ def toRpn(infixStr):
 
     return toRpn2(tokens, 0)
 
+
+def prepare_param_num(param):
+    print('\tMOVE %D ' + param + ', R0', sep='')
+    print('\tPUSH R0')
+    return
+
 def evaluate(expression):
     expression = expression.split()
     stack = []
 
     for ele in expression:
         if ele not in '/*+-':
-            stack.append(int(ele))
+            stack.append(ele)
         else:
             right = stack.pop()
             left = stack.pop()
 
-            print('\tMOVE %D ' + str(left) + ', R0', sep='')
-            print('\tPUSH R0')
-            print('\tMOVE %D ' + str(right) + ', R0', sep='')
-            print('\tPUSH R0')
+            if left.isdigit():
+                prepare_param_num(left)
+            if right.isdigit():
+                prepare_param_num(right)
 
             if ele == '+':
                 print('\tPOP R0')
                 print('\tPOP R1')
                 print('\tADD R0, R1, R2')
                 print('\tPUSH R2')
-                stack.append(left + right)
+                stack.append('result')
             elif ele == '-':
                 print('\tPOP R0')
                 print('\tPOP R1')
                 print('\tSUB R1, R0, R2')
                 print('\tPUSH R2')
-                stack.append(left - right)
+                stack.append('result')
             elif ele == '*':
                 print('\tCALL MUL')
-                stack.append(left * right)
+                stack.append('result')
             elif ele == '/':
                 print('\tCALL DIV')
-                stack.append(int(left / right))
-    return stack.pop()
+                stack.append('result')
+    #return stack.pop()
 
 
 extract_pj()
@@ -126,6 +133,7 @@ for i in range(1, len(data)):
 
             else:
                 rpn = toRpn(pj[int(row) - 1].split('=')[1].strip())
+                #print(rpn)
 
                 if len(rpn.split(' ')) == 1:   # imamo samo slučaj pridruživanja konstante varijabli
                     # MOVE %D 3, R0
@@ -150,7 +158,9 @@ print('\tHALT\n')
 for v in print_variables:
     print(v)
 
-print('\n' + content)
+print('\n')
+for c in content:
+    print(c)
 
 sys.stdout.close()
 
