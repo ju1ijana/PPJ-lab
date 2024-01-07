0145b4d4890e1ec39392dffa22a019506d2b24d1
unknown <julijana.kolarec@fer.hr>
2024-01-07 21:08:04 +0100

    16/17 ne radi samo ugniježđena

--- FRISCGenerator2.py
+++ FRISCGenerator2.py
@@ -20,7 +20,7 @@ print("\tMOVE 40000, R7 ; init stog\n")
 print_variables = []
 
 data = sys.stdin.read().split("\n")
-#data = ['<program>', ' <lista_naredbi>', '  <naredba>', '   <naredba_pridruzivanja>', '    IDN 1 i', '    OP_PRIDRUZI 1 =', '    <E>', '     <T>', '      <P>', '       BROJ 1 0', '      <T_lista>', '       $', '     <E_lista>', '      $', '  <lista_naredbi>', '   <naredba>', '    <naredba_pridruzivanja>', '     IDN 2 rez', '     OP_PRIDRUZI 2 =', '     <E>', '      <T>', '       <P>', '        OP_MINUS 2 -', '        <P>', '         BROJ 2 12', '       <T_lista>', '        $', '      <E_lista>', '       $', '   <lista_naredbi>', '    <naredba>', '     <za_petlja>', '      KR_ZA 3 za', '      IDN 3 i', '      KR_OD 3 od', '      <E>', '       <T>', '        <P>', '         OP_MINUS 3 -', '         <P>', '          BROJ 3 1', '        <T_lista>', '         $', '       <E_lista>', '        $', '      KR_DO 3 do', '      <E>', '       <T>', '        <P>', '         OP_MINUS 3 -', '         <P>', '          BROJ 3 1', '        <T_lista>', '         $', '       <E_lista>', '        $', '      <lista_naredbi>', '       <naredba>', '        <naredba_pridruzivanja>', '         IDN 4 rez', '         OP_PRIDRUZI 4 =', '         <E>', '          <T>', '           <P>', '            IDN 4 rez', '           <T_lista>', '            $', '          <E_lista>', '           OP_MINUS 4 -', '           <E>', '            <T>', '             <P>', '              IDN 4 i', '             <T_lista>', '              $', '            <E_lista>', '             $', '       <lista_naredbi>', '        $', '      KR_AZ 5 az', '    <lista_naredbi>', '     $']
+#data = ['<program>', ' <lista_naredbi>', '  <naredba>', '   <naredba_pridruzivanja>', '    IDN 1 n', '    OP_PRIDRUZI 1 =', '    <E>', '     <T>', '      <P>', '       BROJ 1 10', '      <T_lista>', '       $', '     <E_lista>', '      $', '  <lista_naredbi>', '   <naredba>', '    <naredba_pridruzivanja>', '     IDN 2 rez', '     OP_PRIDRUZI 2 =', '     <E>', '      <T>', '       <P>', '        BROJ 2 0', '       <T_lista>', '        $', '      <E_lista>', '       $', '   <lista_naredbi>', '    <naredba>', '     <za_petlja>', '      KR_ZA 3 za', '      IDN 3 n', '      KR_OD 3 od', '      <E>', '       <T>', '        <P>', '         BROJ 3 1', '        <T_lista>', '         $', '       <E_lista>', '        $', '      KR_DO 3 do', '      <E>', '       <T>', '        <P>', '         BROJ 3 5', '        <T_lista>', '         $', '       <E_lista>', '        $', '      <lista_naredbi>', '       <naredba>', '        <naredba_pridruzivanja>', '         IDN 4 rez', '         OP_PRIDRUZI 4 =', '         <E>', '          <T>', '           <P>', '            IDN 4 rez', '           <T_lista>', '            $', '          <E_lista>', '           OP_PLUS 4 +', '           <E>', '            <T>', '             <P>', '              IDN 4 n', '             <T_lista>', '              $', '            <E_lista>', '             $', '       <lista_naredbi>', '        $', '      KR_AZ 5 az', '    <lista_naredbi>', '     <naredba>', '      <naredba_pridruzivanja>', '       IDN 6 rez', '       OP_PRIDRUZI 6 =', '       <E>', '        <T>', '         <P>', '          IDN 6 rez', '         <T_lista>', '          $', '        <E_lista>', '         OP_PLUS 6 +', '         <E>', '          <T>', '           <P>', '            IDN 6 n', '           <T_lista>', '            $', '          <E_lista>', '           $', '     <lista_naredbi>', '      <naredba>', '       <za_petlja>', '        KR_ZA 7 za', '        IDN 7 n', '        KR_OD 7 od', '        <E>', '         <T>', '          <P>', '           BROJ 7 1', '          <T_lista>', '           $', '         <E_lista>', '          $', '        KR_DO 7 do', '        <E>', '         <T>', '          <P>', '           BROJ 7 5', '          <T_lista>', '           $', '         <E_lista>', '          $', '        <lista_naredbi>', '         <naredba>', '          <naredba_pridruzivanja>', '           IDN 8 rez', '           OP_PRIDRUZI 8 =', '           <E>', '            <T>', '             <P>', '              IDN 8 rez', '             <T_lista>', '              $', '            <E_lista>', '             OP_PLUS 8 +', '             <E>', '              <T>', '               <P>', '                IDN 8 n', '               <T_lista>', '                $', '              <E_lista>', '               $', '         <lista_naredbi>', '          $', '        KR_AZ 9 az', '      <lista_naredbi>', '       $']
 
 data.pop()
 
@@ -102,8 +102,6 @@ def find_version_of_var(var):
     versions = [element for element in variables if element.startswith(var)]
     return max(versions)
 
-
-
 # preuzeto sa https://www.geeksforgeeks.org/evaluate-the-value-of-an-arithmetic-expression-in-reverse-polish-notation-in-python/
 def evaluate(expression):
     expression = expression.split()
@@ -159,6 +157,8 @@ variables = []  # za spremanje varijabli koje su deklarirane
 
 counters = []
 
+used_loop_names = []
+
 for i, line in enumerate(pj):
     if line.find('za') != -1:
         in_a_loop += 1
@@ -172,9 +172,20 @@ for i, line in enumerate(pj):
             prepare_param_num_neg(od)
         else:  # deklaracija varijable
             rpn = toRpn(od)
-            declare(rpn)
-        dos['do' + str(in_a_loop)] = line.split('do')[1].strip()  # spremamo za kasnije
-        print('L' + str(in_a_loop), end='')
+            if len(rpn) == 1 and not rpn.isdigit():
+                print('\tLOAD R0, (' + find_version_of_var(od) + ')')
+                print('\tSTORE R0, (' + counters[-1] + ')')
+            else:
+                declare(rpn)
+
+        i = 0
+        while 'L' + str(in_a_loop + i) in used_loop_names:
+            i += 1
+        loop_name = 'L' + str(in_a_loop + i)
+        used_loop_names.append(loop_name)
+        print(loop_name, end='')
+        dos['do' + str(in_a_loop)] = [line.split('do')[1].strip(), loop_name]  # spremamo za kasnije
+
     if line.find('az') != -1:
         # pronađi counter trenutne petlje i povećaj ga
         counter = [c for c in counters if c.endswith(str(in_a_loop))][0]
@@ -182,7 +193,7 @@ for i, line in enumerate(pj):
         print('\tADD R0, 1, R0')
         print('\tSTORE R0, (' + counter + ')')
         # računamo vrijednost do i računamo da se nalazi na stogu
-        do = dos['do' + str(in_a_loop)]
+        do, loop_name = dos['do' + str(in_a_loop)]
         if bool(re.match(r'^-\d+$', do)):  # imamo "do -1"
             print('\tMOVE %D ' + do[1:] + ', R0', sep='')
             print('\tPUSH R0')
@@ -190,15 +201,14 @@ for i, line in enumerate(pj):
             print('\tMOVE %D 0, R1')
             print('\tSUB R1, R0, R2')
             print('\tPUSH R2')
-        else:  # deklaracija varijable
+        else:
             rpn = toRpn(do)
-            if len(rpn.split(' ')) == 1 and rpn.isdigit():  # imamo samo slučaj pridruživanja konstante varijabli
+            if len(rpn.split(' ')) == 1 and rpn.isdigit():
                 print('\tMOVE %D ' + rpn + ', R0', sep='')
                 print('\tPUSH R0')
-
             else:
                 if len(rpn) == 1:
-                    print('\tLOAD R0, (' + [element for element in variables if element.startswith(do)][-1] + ')')
+                    print('\tLOAD R0, (' + find_version_of_var(do) + ')')
                     print('\tPUSH R0')
                 else:
                     evaluate(rpn)
@@ -207,7 +217,7 @@ for i, line in enumerate(pj):
         print('\tPOP R1')
         # uvjetni skok
         print('\tCMP R0, R1')
-        print('\tJP_SLE L' + str(in_a_loop))
+        print('\tJP_SLE ' + loop_name)
 
         variables = [item for item in variables if not item.endswith(str(in_a_loop))]
         in_a_loop -= 1
@@ -230,7 +240,7 @@ for i, line in enumerate(pj):
 print('\tLOAD R6, (rez0)')
 print('\tHALT\n')
 
-for v in print_variables:
+for v in set(print_variables):
     print(v)
 
 print('\n')
